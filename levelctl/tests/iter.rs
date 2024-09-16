04923f3448927132054e2750de98fd90b16cff2e
chenmortal <2584139809@qq.com>
2024-09-17 00:52:30 +0800

    fix(levelctl): add test iter add meta

--- levelctl/tests/iter.rs
+++ levelctl/tests/iter.rs
@@ -1,4 +1,5 @@
 use bytesize::ByteSize;
+use mors_common::kv::Meta;
 use mors_encrypt::cipher::AesCipher;
 use mors_sstable::test_utils::generate_table;
 use mors_traits::iter::{
@@ -16,10 +17,11 @@ async fn test_cache_table_concat_iter() {
         ByteSize::mib(2).as_u64() as usize,
         "k",
         "v",
+        Meta::default(),
     )
     .await;
     let mut iter = CacheTableConcatIter::new(tables, true);
-    let kvs = generate_kv_slice(range, "k", "v");
+    let kvs = generate_kv_slice(range, "k", "v", Meta::default());
     for (k, v) in kvs {
         match iter.next() {
             Ok(b) => {
@@ -43,6 +45,7 @@ async fn test_cache_merge_iter() {
         ByteSize::mib(2).as_u64() as usize,
         "k",
         "v",
+        Meta::default(),
     )
     .await;
     let other = tables.drain(1..).collect::<Vec<_>>();
@@ -53,7 +56,7 @@ async fn test_cache_merge_iter() {
         Box::new(other),
     ])
     .unwrap();
-    let kvs = generate_kv_slice(range, "k", "v");
+    let kvs = generate_kv_slice(range, "k", "v", Meta::default());
     for (k, v) in kvs {
         match iter.next() {
             Ok(b) => {
